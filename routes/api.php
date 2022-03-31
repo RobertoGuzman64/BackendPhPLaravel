27eafb7056884dd531ac327af3e5e28cbfda802f
RobertoGuzman <robertoguzman64@gmail.com>
2022-03-31 11:26:49 +0200

    feat: Comenzando el proyecto con la estructura básica

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Enlace a la clase que contiene los métodos de la API HomeController
+use App\Http\Controllers\AuthController;
+
+
+//**************************//
+//ENDPOINTS DE AUTENTICACIÓN//
+//**************************//
+
+
+
+
+
+
+
+
+
