f5beafb1de4fa024380d41a0bbda8fe52f002f67
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-06 19:14:54 +0200

    feat: Añadido el método de mostrar todas las partidas en PartidaController

--- app/Http/Controllers/PartidaController.php
+++ app/Http/Controllers/PartidaController.php
@@ -1,10 +1,23 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use App\Models\Partida;
 
 class PartidaController extends Controller
 {
-    //
+    // METODO DE MOSTRAR TODAS LAS PARTIDAS
+    public function GETpartidas(){
+        try{
+            $partidas = Partida::all();
+            return $partidas;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
+            }
+        }
+    }
+
+
 }
