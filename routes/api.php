23bf7430a9e87068f03f0f091a8740965c3b4355
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-06 18:21:05 +0200

    fix:Corregido el login que habia que sacarlo del endpoint y añadidos comentarios de el objetivo del proyecto en api.php

--- routes/api.php
+++ routes/api.php
@@ -12,11 +12,12 @@ use App\Http\Controllers\AuthController;
 //**************************//
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::group([
     'middleware' => 'jwt.auth'
 ], function () {
-    Route::post('/login', [AuthController::class, 'login']);
+    
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'me']);
 });
@@ -25,4 +26,31 @@ Route::group([
 
 
 
+// Los requisitos funcionales de la aplicación son los siguientes:
+// ● RF.1 Los usuarios se tienen que poder registrar a la aplicación,
+// estableciendo un usuario/contraseña.
 
+// ● RF.2 Los usuarios tienen que autenticarse a la aplicación haciendo login.
+
+// ● RF.3 Los usuarios tienen que poder crear Partídas (grupos) para
+// un determinado videojuego.
+
+// ● RF.4 Los usuarios tienen que poder buscar Partídas seleccionando
+// un videojuego.
+
+// Prueba técnica
+
+// 2
+
+// ● RF.5 Los usuarios pueden entrar y salir de una Party.
+
+// ● RF.6 Los usuarios tienen que poder enviar mensajes a la Party. Estos
+// mensajes tienen que poder ser editados y borrados por su usuario creador.
+
+// ● RF.7 Los mensajes que existan en una Party se tienen que visualizar como
+// un chat común.
+
+// ● RF.8 Los usuarios pueden introducir y modificar sus datos de perfil, por
+// ejemplo, su usuario de Steam.
+
+// ● RF.9 Los usuarios tienen que poder hacer logout de la aplicación web.
