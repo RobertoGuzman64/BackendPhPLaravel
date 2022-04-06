430157c2e8ab40e7e04f830217c91b03a2bac557
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-06 18:40:15 +0200

    feat: Añadidos los campos requeridos en el modelo Partida.php y añadidas las relaciones entre las futuras tablas qu van a usar la aplicación

--- app/Models/Partida.php
+++ app/Models/Partida.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Partida extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        "name",
+        "propietarioId",
+        "juegoId",
+    ];
+
+    public function juego()
+    {
+        return $this->belongsTo(Juego::class);
+    }
+
+    public function jugadores()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function mensajes()
+    {
+        return $this->hasMany(Mensaje::class);
+    }
 }
