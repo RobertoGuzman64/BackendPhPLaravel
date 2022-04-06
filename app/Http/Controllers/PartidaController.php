251160053b5221d9136efc64b195b16190578582
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-06 19:27:30 +0200

    feat: Añadido el método do de crear una partida en PartidaController.php

--- app/Http/Controllers/PartidaController.php
+++ app/Http/Controllers/PartidaController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 use App\Models\Partida;
@@ -7,17 +9,37 @@ use App\Models\Partida;
 class PartidaController extends Controller
 {
     // METODO DE MOSTRAR TODAS LAS PARTIDAS
-    public function GETpartidas(){
-        try{
+    public function GETmostrarPartidas()
+    {
+        try {
             $partidas = Partida::all();
             return $partidas;
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
     }
-
-
+    // METODO DE CREAR PARTIDA
+    public function POSTcrearPartida(Request $request)
+    {
+        $name = $request->input('name');
+        $propietarioId = $request->input('propietarioId');
+        $juegoId = $request->input('juegoId');
+        try {
+            return Partida::create(
+                [
+                    'name' => $name,
+                    'propietarioId' => $propietarioId,
+                    'juegoId' => $juegoId
+                ]
+            );
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
 }
