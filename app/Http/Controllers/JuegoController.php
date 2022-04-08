aabcc4306026493bbdc7fa82a5caa308d6b3be24
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-08 14:17:45 +0200

    feat: Creado el método de mostrar Juegos en JuegoController.php

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Database\QueryException;
+use App\Models\Juego;
 use Illuminate\Http\Request;
 
 class JuegoController extends Controller
 {
-    //
+    // METODO DE MOSTRAR TODOS LOS JUEGOS
+    public function GETmostrarJuegos()
+    {
+        try {
+            $juegos = Juego::all();
+            return $juegos;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
+    // METODO DE CREAR JUEGO
+    
 }
