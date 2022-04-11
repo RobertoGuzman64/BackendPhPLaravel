0b5256c2b0c3f819c6455d864244915a909ededa
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-11 13:37:12 +0200

    feat: Añadido nuevo método de actualizar un Juego en JuegoController.php

--- app/Http/Controllers/JuegoController.php
+++ app/Http/Controllers/JuegoController.php
@@ -43,5 +43,38 @@ class JuegoController extends Controller
         }
     }
     // METODO DE MOSTRAR UN JUEGO POR ID
-    
+    public function POSTmostrarJuegoId(Request $request)
+    {
+        $id = $request->input('id');
+        try {
+            $juego = Juego::find($id);
+            return $juego;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
+    // METODO DE ACTUALIZAR UN JUEGO
+    public function PUTactualizaJuego(Request $request)
+    {
+        $id = $request->input('id');
+        $titulo = $request->input('titulo');
+        $imagenJuegoURL = $request->input('imagenJuegoURL');
+        $juegoURL = $request->input('juegoURL');
+        try {
+            $juego = Juego::find($id);
+            $juego->titulo = $titulo;
+            $juego->imagenJuegoURL = $imagenJuegoURL;
+            $juego->juegoURL = $juegoURL;
+            $juego->save();
+            return $juego;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
 }
