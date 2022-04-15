a8ea3be7081fe109ed181bdac2b84211c4c89b5c
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-15 18:56:03 +0200

    feat: Creados los campos necesarios y las relaciones del modelo Jugador en Jugador.php

--- app/Models/Jugador.php
+++ app/Models/Jugador.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Jugador extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'partidaId',
+        'usuarioId',
+    ];
+    
+    public function partidas()
+    {
+        return $this->belongsTo(Partida::class);
+    }
+
+    public function usuarios()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
+    
 }
