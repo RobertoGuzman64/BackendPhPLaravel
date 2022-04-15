afd3bc79983cdfb7cfdeb5cd1404346aef9afb8e
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-15 16:35:27 +0200

    feat: Añadido nuevo método GETmostrarMensajes en MensajeController.php

--- app/Http/Controllers/MensajeController.php
+++ app/Http/Controllers/MensajeController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use App\Models\Mensaje;
 
 class MensajeController extends Controller
 {
-    //
+    // METODO DE MOSTRAR TODOS LOS MENSAJES
+    public function GETmostrarMensajes()
+    {
+        try {
+            $mensajes = Mensaje::all();
+            return $mensajes;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
 }
