ce08a9866a1f7fcd2e3aed3adba4e5d234578b2a
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-15 19:56:19 +0200

    feat: Añadido nuevo endpoint PUTactualizaJugador en JugadorController.php

--- app/Http/Controllers/JugadorController.php
+++ app/Http/Controllers/JugadorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 use App\Models\Jugador;
@@ -8,19 +9,21 @@ use App\Models\Jugador;
 class JugadorController extends Controller
 {
     // METODO DE MOSTRAR TODOS LOS JUGADORES
-    public function GETmostrarJugadores(){
+    public function GETmostrarJugadores()
+    {
         try {
             $jugadores = Jugador::all();
             return $jugadores;
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
     }
     // METODO DE CREAR UN JUGADOR
-    public function POSTcrearJugador(Request $request){
+    public function POSTcrearJugador(Request $request)
+    {
         $partidaId = $request->input('partidaId');
         $usuarioId = $request->input('usuarioId');
         try {
@@ -32,46 +35,70 @@ class JugadorController extends Controller
             );
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
     }
     // METODO DE MOSTRAR UN JUGADOR POR ID
-    public function POSTmostrarJugadorId(Request $request){
+    public function POSTmostrarJugadorId(Request $request)
+    {
         $jugadorId = $request->input('jugadorId');
         try {
             $jugador = Jugador::find($jugadorId);
             return $jugador;
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
     }
     // METODO DE MOSTRAR UN JUGADOR POR PARTIDA ID
-    public function POSTmostrarJugadorPartidaId(Request $request){
+    public function POSTmostrarJugadorPartidaId(Request $request)
+    {
         $partidaId = $request->input('partidaId');
         try {
             $jugador = Jugador::where('partidaId', $partidaId)->get();
             return $jugador;
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
     }
     // METODO DE MOSTRAR UN JUGADOR POR USUARIO ID
-    public function POSTmostrarJugadorUsuarioId(Request $request){
+    public function POSTmostrarJugadorUsuarioId(Request $request)
+    {
         $usuarioId = $request->input('usuarioId');
         try {
             $jugador = Jugador::where('usuarioId', $usuarioId)->get();
             return $jugador;
         } catch (QueryException $error) {
             $codigoError = $error->errorInfo[1];
-            if($codigoError){
+            if ($codigoError) {
+                return "Error $codigoError";
+            }
+        }
+    }
+    // METODO DE ACTUALIZAR UN JUGADOR
+    public function PUTactualizaJugador(Request $request)
+    {
+        $jugadorId = $request->input('jugadorId');
+        $partidaId = $request->input('partidaId');
+        $usuarioId = $request->input('usuarioId');
+        try {
+            $jugador = Jugador::where('jugadorId', '=', $jugadorId)->update(
+                [
+                    'partidaId' => $partidaId,
+                    'usuarioId' => $usuarioId
+                ]
+            );
+            return Jugador::all()->where('jugadorId', '=', $jugadorId);
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if ($codigoError) {
                 return "Error $codigoError";
             }
         }
