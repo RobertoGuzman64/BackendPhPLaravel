fd86c1ee7049b0a713b2d71e0c3dd665aa604e4f
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-15 19:11:30 +0200

    feat: Añadido nuevo método GETmostrarJugadores en JugadorController.php

--- app/Http/Controllers/JugadorController.php
+++ app/Http/Controllers/JugadorController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use App\Models\Jugador;
 
 class JugadorController extends Controller
 {
-    //
+    // METODO DE MOSTRAR TODOS LOS JUGADORES
+    public function GETmostrarJugadores(){
+        try {
+            $jugadores = Jugador::all();
+            return $jugadores;
+        } catch (QueryException $error) {
+            $codigoError = $error->errorInfo[1];
+            if($codigoError){
+                return "Error $codigoError";
+            }
+        }
+    }
 }
