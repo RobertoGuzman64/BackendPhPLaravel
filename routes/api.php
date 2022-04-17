3a3c83bc9740ba36067215f830f9e4be255b59a1
RobertoGuzman <robertoguzman64@gmail.com>
2022-04-17 20:53:43 +0200

    improvement: Eliminados comentarios sobrantes de api.php

--- routes/api.php
+++ routes/api.php
@@ -53,38 +53,3 @@ Route::post('/jugadorPartidaId', [JugadorController::class, "POSTmostrarJugadorP
 Route::post('/jugadorUsuarioId', [JugadorController::class, "POSTmostrarJugadorUsuarioId"]);
 Route::put('/jugadorActualiza', [JugadorController::class, "PUTactualizaJugador"]);
 Route::delete('/jugadorBorrar', [JugadorController::class, "DELETEborrarJugador"]);
-
-
-
-
-
-
-    
-// Los requisitos funcionales de la aplicación son los siguientes:
-// ● RF.1 Los usuarios se tienen que poder registrar a la aplicación,
-// estableciendo un usuario/contraseña.
-
-// ● RF.2 Los usuarios tienen que autenticarse a la aplicación haciendo login.
-
-// ● RF.3 Los usuarios tienen que poder crear Partídas (grupos) para
-// un determinado videojuego.
-
-// ● RF.4 Los usuarios tienen que poder buscar Partídas seleccionando
-// un videojuego.
-
-// Prueba técnica
-
-// 2
-
-// ● RF.5 Los usuarios pueden entrar y salir de una Party.
-
-// ● RF.6 Los usuarios tienen que poder enviar mensajes a la Party. Estos
-// mensajes tienen que poder ser editados y borrados por su usuario creador.
-
-// ● RF.7 Los mensajes que existan en una Party se tienen que visualizar como
-// un chat común.
-
-// ● RF.8 Los usuarios pueden introducir y modificar sus datos de perfil, por
-// ejemplo, su usuario de Steam.
-
-// ● RF.9 Los usuarios tienen que poder hacer logout de la aplicación web.
